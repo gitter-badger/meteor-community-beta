303e1ac511c0bd9abf87f22b9b8878d0322c96dc
Luke Clifton <luke@propelcreative.co.uk>
2014-09-23 15:45:03 +0100

    first commit for meteor.community project! basic app scaffold and styling set-up

---
+++ both/router.js
@@ -0,0 +1,47 @@
+/*****************************************************************************/
+/* Router
+/*****************************************************************************/
+
+Router.configure(
+{
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+});
+
+Router.map(function() 
+{
+    this.route('home', 
+    {
+        path: '/',
+    });
+
+    this.route('home', 
+    {
+        path: '/home',
+    });
+
+    this.route('forum', 
+    {
+        path: '/forum',
+    });
+
+    this.route('developers', 
+    {
+        path: '/developers',
+    });
+
+    this.route('jobs', 
+    {
+        path: '/jobs',
+    });
+
+    this.route('chat', 
+    {
+        path: '/chat',
+    });
+
+    this.route('resources', 
+    {
+        path: '/resources',
+    });
+});
